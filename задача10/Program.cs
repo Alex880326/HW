d316f5149986fb581f5da735cf1843b95a37fd10
Alex880326 <124200830+Alex880326@users.noreply.github.com>
2023-02-14 12:52:34 +0300

    Update Program.cs

--- задача10/Program.cs
+++ задача10/Program.cs
@@ -3,7 +3,7 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-System.Console.WriteLine("Введите число ");
+System.Console.WriteLine("Введите трехзначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int number2 = number % 100;
 int number3 = number2 /10;
@@ -14,6 +14,6 @@ if (number < 1000)
 }
 else
 {
-    System.Console.WriteLine("неверно");
+    System.Console.WriteLine("Число не трехзначное, попробуйте еще раз ");
 }
 
